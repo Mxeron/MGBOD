4e8d5d57b1ddee3c186cca6893388b54f4f36913
Mxeron <36753564+Mxeron@users.noreply.github.com>
2024-07-15 11:11:26 +0800

    Update GB_generation_with_idx.py

--- Code/GB_generation_with_idx.py
+++ Code/GB_generation_with_idx.py
@@ -18,13 +18,11 @@ from scipy.spatial.distance import cdist
 
 warnings.filterwarnings('ignore')
 
-
 def get_radius(gb):
     real_gb = gb[:,:-1]
     center = real_gb.mean(0)
     radius = max(np.sqrt(np.sum((real_gb - center) ** 2, axis=1)))
     return radius
-
 
 # 2-means
 def spilt_ball(data):
@@ -33,7 +31,6 @@ def spilt_ball(data):
     ball1 = data[cluster == 0, :]
     ball2 = data[cluster == 1, :]
     return [ball1, ball2]
-
 
 # 根据距离快速划分粒球
 def spilt_ball_2(data):
@@ -44,9 +41,8 @@ def spilt_ball_2(data):
     r, c = np.where(D == np.max(D))
     r1 = r[1]
     c1 = c[1]
-    # 上面找到粒球中距离最远的两个样本
     for j in range(0, len(data)):
-        if D[j, r1] < D[j, c1]:  # 当前样本与样本r1、c1的距离
+        if D[j, r1] < D[j, c1]:
             ball1.extend([data[j, :]])
         else:
             ball2.extend([data[j, :]])
@@ -72,10 +68,8 @@ def get_density_volume(gb):
 def division_ball(gb_list):
     gb_list_new = []
     for gb in gb_list:
-        if len(gb) >= 4:  # 8 or 10?
-            # ball_1, ball_2 = spilt_ball(gb)
+        if len(gb) >= 4:
             ball_1, ball_2 = spilt_ball_2(gb)
-            # 标称数据存在问题
             if len(ball_1) == 0 or len(ball_2) == 0:
                 gb_list_new.append(gb)
                 continue
@@ -86,9 +80,7 @@ def division_ball(gb_list):
             w1 = len(ball_1) / w
             w2 = len(ball_2) / w
             w_child = (w1 * density_child_1 + w2 * density_child_2)
-            # t1 = ((density_child_1 > density_parent) & (density_child_2 > density_parent))
             t2 = (w_child > density_parent)  # 大于父球的密度才划分，否则不划分
-            # t3 = ((len(ball_1) > 4) & (len(ball_2) > 4))
             if t2:
                 gb_list_new.extend([ball_1, ball_2])
             else:
@@ -105,7 +97,6 @@ def normalized_ball(gb_list, radius_detect):
         if len(gb) < 2:
             gb_list_temp.append(gb)
         else:
-            # ball_1, ball_2 = spilt_ball(gb)
             ball_1, ball_2 = spilt_ball_2(gb)
             if get_radius(gb) <= 2 * radius_detect:
                 gb_list_temp.append(gb)
@@ -117,28 +108,22 @@ def normalized_ball(gb_list, radius_detect):
 
 def get_GB(data):
     data_num = data.shape[0]
-    index = np.array(range(data_num)).reshape(data_num, 1)  # 创建一个索引列, 从0开始的
-    data = np.hstack((data, index))  # 在数据的最后一列添加索引列
+    index = np.array(range(data_num)).reshape(data_num, 1)
+    data = np.hstack((data, index))
     gb_list_temp = [data]
-
-    # row = np.shape(gb_list_temp)[0]
-    # col = np.shape(gb_list_temp)[1]
-    # n = row * col
-
-    # plot_dot(data)
 
     '''
     先生成粒球
     '''
     while 1:
         ball_number_old = len(gb_list_temp)
-        gb_list_temp = division_ball(gb_list_temp)  # 质量轮
+        gb_list_temp = division_ball(gb_list_temp)
         ball_number_new = len(gb_list_temp)
         if ball_number_new == ball_number_old:
             break
     radius = []
     for gb in gb_list_temp:
-        if len(gb) >= 2:  # 粒球中至少要有2个点
+        if len(gb) >= 2:
             radius.append(get_radius(gb))
 
     radius_median = np.median(radius)
@@ -155,38 +140,6 @@ def get_GB(data):
         if ball_number_new == ball_number_old:
             break
 
-    # plot_dot(data)
-    # draw_ball(gb_list_temp)
 
     gb_list_final = gb_list_temp
     return gb_list_final
-
-if __name__ == '__main__':
-    load_data = loadmat(r"D:\outlier_datasets\Numerical\diabetes_tested_positive_26_variant1.mat")  # 加载数据集
-    # load_data = loadmat('Lymphography.mat')
-    data = load_data['trandata']
-    n, m = data.shape
-    print('样本个数：', n, '总属性个数', m)
-    label = data[:, m - 1]
-    print('异常点个数:', label.sum())
-    trandata = data[:, :m - 1]
-    scaler = MinMaxScaler()
-    trandata = scaler.fit_transform(trandata)
-
-    start_time = time.time()
-    # 还没有把index带上！
-    gb_final = get_GB(trandata)
-    end_time = time.time()
-    print("运行时间：", end_time - start_time)
-    print("粒球个数：", len(gb_final))
-
-    # idx_check = []
-    # for i in gb_final:
-    #     idx_check.extend(i[:,-1])
-    #     print(i[:,-1])
-    #     print('******')
-    # idx_check = sorted(idx_check)
-    # print(idx_check)
-    # print(gb_final[0])
-    # # gb center
-    # print(np.mean(gb_final[0][:,:-1], axis=0))
